dd583278eb61fe9b9754bb36de550bbb35511f49
maxwell walin <maxwellwalin@gmail.com>
2022-01-08 18:24:49 -0800

    redesigned hero and added responsiveness

--- components/Hero.js
+++ components/Hero.js
@@ -1,102 +1,52 @@
-import styled from 'styled-components'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from 'react-responsive-carousel'
 
 export default function Hero() {
     return (
-        <div id='slider'>
-            <figure>
-                <div className='sliderImg tacoTuesdayImg'>
-                    <div />
-                    <div className='sliderText'>
-                        <h2>Taco Tuesday</h2>
-                        <p>Stop by for Taco Tuesday! Get any Carne Asada, Carnitas, Pollo, and Al Pastor tacos for $1.99 each!</p>
-                        <div>
-                            {new Date().getDay() === 2 && <button>Order Now</button>}
-                            <button>View Menu</button>
-                        </div>
+        <Carousel autoPlay dynamicHeight infiniteLoop showThumbs={false} swipeable interval={5000}>
+            <div className='sliderImg tacoTuesdayImg'>
+                <div />
+                <div className='sliderText'>
+                    <h2>Taco Tuesday</h2>
+                    <p>Stop by for Taco Tuesday! Get any Carne Asada, Carnitas, Pollo, and Al Pastor tacos for $1.99 each!</p>
+                    <div>
+                        {new Date().getDay() === 2 && <a href="/order"><button>Order Now</button></a>}
+                        <a href="/images/menu.pdf">View Menu</a>
                     </div>
                 </div>
-                <div className='sliderImg sundayBrunchImg'>
-                    <div />
-                    <div className='sliderText'>
-                        <h2>Sunday Brunch</h2>
-                        <p>Every Sunday, all breakfast burritos are only $6!</p>
-                        <div>
-                            {new Date().getDay() === 0 && <button>Order Now</button>}
-                            <button>View Menu</button>
-                        </div>
+            </div>
+            <div className='sliderImg sundayBrunchImg'>
+                <div />
+                <div className='sliderText'>
+                    <h2>Sunday Brunch</h2>
+                    <p>Every Sunday, all breakfast burritos are only $6!</p>
+                    <div>
+                        {new Date().getDay() === 0 && <a href="/order">Order Now</a>}
+                        <a href="/images/menu.pdf">View Menu</a>
                     </div>
                 </div>
-                <div className='sliderImg happyHourImg'>
-                    <div />
-                    <div className='sliderText'>
-                        <h2>Happy Hour</h2>
-                        <p>Every Friday and Saturday blah blah. View our selection of drinks!</p>
-                        <div>
-                            <button>View Menu</button>
-                        </div>
+            </div>
+            <div className='sliderImg happyHourImg'>
+                <div />
+                <div className='sliderText'>
+                    <h2>Happy Hour</h2>
+                    <p>Every Friday and Saturday blah blah. View our selection of drinks!</p>
+                    <div>
+                        <a href="/images/menu.pdf">View Menu</a>
                     </div>
                 </div>
-                <div className='sliderImg fourthPromotionImg'>
-                    <div />
-                    <div className='sliderText'>
-                        <h2>Monday Madness</h2>
-                        <p>Stop by for Taco Tuesday! Get any Carne Asada, Carnitas, Pollo, and Al Pastor tacos for $1.99 each!</p>
-                        <div>
-                            <button>Order Now</button>
-                            <button>View Menu</button>
-                        </div>
+            </div>
+            <div className='sliderImg fourthPromotionImg'>
+                <div />
+                <div className='sliderText'>
+                    <h2>Monday Madness</h2>
+                    <p>Stop by for Taco Tuesday! Get any Carne Asada, Carnitas, Pollo, and Al Pastor tacos for $1.99 each!</p>
+                    <div>
+                        <a href="/order">Order Now</a>
+                        <a href="/images/menu.pdf">View Menu</a>
                     </div>
                 </div>
-                <div className='sliderImg tacoTuesdayImg'>
-                    <div />
-                    <div className='sliderText'>
-                        <h2>Taco Tuesday</h2>
-                        <p>Stop by for Taco Tuesday! Get any Carne Asada, Carnitas, Pollo, and Al Pastor tacos for $1.99 each!</p>
-                        <div>
-                            {new Date().getDay() === 2 && <button>Order Now</button>}
-                            <button>View Menu</button>
-                        </div>
-                    </div>
-                </div>
-            </figure>
-        </div>
+            </div>
+        </Carousel >
     )
 }
-
-const Slider = styled.div`
-    overflow: hidden;
-`
-
-const Figure = styled.figure`
-    position: relative;
-    width: 500%;
-    margin: 0;
-    left: 0;
-    animation: 20s silder infinite;
-`
-
-const SliderImage = styled.img`
-    width: 20%;
-    float: left;
-`
-
-const HeroDiv = styled.div`
-    height: 70vh;
-    width: inherit;
-    background-image: url('https://res.cloudinary.com/dtpgzynwd/image/upload/v1641181014/o_ohnjc2.jpg');
-    background-attachment: fixed;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    justify-items: center;
-    align-items: center;
-`
-
-{/* <HeroDiv>
-            <div />
-            <div>
-                Taco Tuesday
-            </div>
-        </HeroDiv> */}
--- components/Layout.js
+++ components/Layout.js
@@ -4,18 +4,19 @@ import Footer from "./Footer";
 
 export default function Layout({ children }) {
     return (
-        <div>
+        <>
             <Head>
                 <title>El Mesquitito Grill | Authentic Mexican Food Restaurant in Costa Mesa, California</title>
                 <meta name="description" content="El Mesquitito Grill is Costa Mesa, CA's best stop for reasonably priced, authentic, and delicious mexican food." />
+                <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
                 <link rel="icon" href="/images/logo-square.jpg" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link href="https://fonts.googleapis.com/css2?family=Spectral:wght@300&display=swap" rel="stylesheet" />
             </Head>
             <Header />
             <main>{children}</main>
             <Footer />
-        </div>
+        </>
     )
 }
