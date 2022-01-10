384a74f9c27b518ed50228712d4e7eeaa1719e5f
maxwell walin <maxwellwalin@gmail.com>
2022-01-09 16:53:38 -0800

    updated button size and styling and responsiveness

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,6 @@ class AppDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    <meta name="description" content="El Mesquitito Grill is Costa Mesa, CA's best stop for reasonably priced, authentic, and delicious mexican food." />
                 </Head>
                 <body>
                     <Main></Main>
